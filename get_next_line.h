1a8ba2886285d6b577197c5573f92fc089d14d52
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2025-04-18 11:02:50 +0300

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: erbastug <erbastug@student.42kocaeli.co    +#+  +:+       +#+        */
+/*   By: erbastug <erbastug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/27 21:15:30 by erbastug          #+#    #+#             */
-/*   Updated: 2024/12/30 18:26:02 by erbastug         ###   ########.fr       */
+/*   Created: 2025/01/01 14:25:41 by erbastug          #+#    #+#             */
+/*   Updated: 2025/01/01 16:52:33 by erbastug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stddef.h>
+# include <unistd.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 11
@@ -21,7 +21,7 @@
 
 char	*get_next_line(int fd);
 char	*ft_free(char *reads, char *buff);
-char	find_newline(char *s);
-char	*merge(char *kontrol, char *buff);
-int		ft_strlen(char *str);
+char	ft_find_newline(char *s);
+char	*ft_merge(char *remainder, char *buff);
+ssize_t	ft_strlen(char *str);
 #endif
